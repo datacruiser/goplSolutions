dd2ab49d5e7b310db78e2ac70b7232d68e2cd738
datacruiser <phy.zju@gmail.com>
2019-11-18 19:23:04 +0800

    add ex1.10

---
+++ chapter01/ex1.8/main.go
@@ -0,0 +1,45 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+
+/*
+Exercise 1.8: Modify fetch to add the preﬁx http:// to each argument URL if it is missing.
+You might want to use strings.HasPrefix.
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
+
+//!-
---
+++ chapter01/ex1.9/main.go
@@ -0,0 +1,50 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+
+/*
+Exercise 1.9: Modify fetch to also print the HTTP status code, found in resp.Status.
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("HTTP Status: ", resp.Status)
+		b, err := ioutil.ReadAll(resp.Body)
+
+		//s, err := ioutil.ReadAll(resp.Status)
+		//_ = b + s
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+
+		//fmt.Printf("%s", s)
+	}
+}
+
+//!-
